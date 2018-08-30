324dc1b4d1b8edfc150b9dfa4b9a4b4f0c7622df
Tina <christinawilson6@gmail.com>
2018-08-30 17:22:26 -0400

    updating questions and pictures

--- functions.js
+++ functions.js
@@ -1,24 +1,27 @@
-function buildQuiz () {
-    const output = [];
+import Questions from './questions.js'
 
-questions.forEach ((currentQuestion, questionNumber) => 
+function buildQuiz () {
+  const output = []
+
+  questions.forEach((currentQuestion, questionNumber) => {
     const answers = []
     for (letter in currentQuestion.answers) {
-        answers.push(
+      answers.push(
         `
         <label>
         <input type="radio" name="answer${questionNumber}" id="answer${questionNumber}" value="${letter}">${letter}: ${currentQuestion.answers[letter]}
         </label>
         `
-        )
+      )
     }
     output.push(
-        `
+      `
         <div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>
         `
     )
-)
+  }
+  )
 }
 console.log('hello')
 
@@ -32,3 +35,13 @@ choice.addEventListener('click', function () {
   console.log(answersArr)
   // console.log(_.sum(answersArr))
 })
+
+<div class="question-title"><h2>Question ${questionNumber}</h2></div>
+<div class="question-content">Placeholder text for question</div>
+<ol class="question-answers">
+    <li><input type="radio" name="answerA" id="question${questionNumber}" value="${letter}"><label for "answerA">${letter}: ${currentQuestion.answers[letter]}</label></li>
+    <li><input type="radio" name="answerB" id="question1" value="1"><label for "answerB">AnswerB</label></li>
+    <li><input type="radio" name="answerC" id="question1" value="1"><label for "answerC">AnswerC</label></li>
+    <li><input type="radio" name="answerD" id="question1" value="1"><label for "answerD">AnswerD</label></li> 
+</ol>
+
